62ee135851150a8388c6357c609ad1215d7d4906
LilSPinachBoy1 <sam@moocows.co.uk>
2025-02-28 11:40:58 +0000

    Started working on timer functionality

--- Behind the veil/home_script.js
+++ Behind the veil/home_script.js
@@ -1,8 +1,20 @@
 // Wait until the page has loaded to do anything
 document.addEventListener("DOMContentLoaded", function() {
     const maths_current_time = document.getElementById("ma_tot_curr_week");
+    const maths_button = document.getElementById("ma_start_stop_butt");
 
-    const d = new Date();
-    let time = d.getTime();
+    const d = new Date()
+    let init_time = d.getTime()
+    let time = 0;
+    let running = false;
     maths_current_time.innerText = "Time studied this week: " + time;
+
+    // Listen for the button press
+    document.addEventListener("click", function() {
+        console.log("Maths button pressed");
+        
+        // Toggle the running variable
+        if (running) {running = false;}
+        else {running = true;};
+    });
 });
