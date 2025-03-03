588b0c75b7367a8902d27e113d61c88bd2f8904d
LilSPinachBoy1 <sam@moocows.co.uk>
2025-03-03 09:37:09 +0000

    Basic timer now works

--- Behind the veil/home_script.js
+++ Behind the veil/home_script.js
@@ -3,11 +3,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const maths_current_time = document.getElementById("ma_tot_curr_week");
     const maths_button = document.getElementById("ma_start_stop_butt");
 
-    const d = new Date()
-    let init_time = d.getTime()
+
+    let init_time = Date.now();
     let time = 0;
     let running = false;
-    maths_current_time.innerText = "Time studied this week: " + time;
+    maths_current_time.innerText = "Time studied this week: " + time + " seconds.";
 
     // Listen for the button press
     maths_button.addEventListener("click", function() {
@@ -17,10 +17,17 @@ document.addEventListener("DOMContentLoaded", function() {
         if (running) {
             running = false;
             maths_button.innerText = "Start timer";
+            time_to_add = Date.now() - init_time;
+            seconds = Math.floor(time_to_add / 1000);
+            time += seconds;
+            console.log(time);
         }
         else {
             running = true;
             maths_button.innerText = "Stop timer";
+            init_time = Date.now();
         };
+
+        maths_current_time.innerText = "Time studied this week: " + time + " seconds.";
     });
 });
